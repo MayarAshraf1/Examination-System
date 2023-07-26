62386d4af72dc1297d6fa6a226bd352374d20a5b
mayar <mayarashraf01018@gmail.com>
2023-07-25 23:28:33 -0700

    exam

---
+++ McqExaminationSystem-Back-End/McqExaminationSystem/Controllers/QuestionController.cs
@@ -0,0 +1,71 @@
+﻿using McqExaminationSystem.DTO;
+using McqExaminationSystem.Models;
+using McqExaminationSystem.Repositories.QuestionRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McqExaminationSystem.Controllers
+{
+    [Route("api/question")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionRepository _repository;
+
+        public QuestionController(IQuestionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("insert")]
+        public ActionResult InsertQuestion(QuestionWithExam question)
+        {
+            if (ModelState.IsValid)
+            {
+                var question2 = new Question
+                {
+                    QuestionHeader = question.QuestionHeader,
+                    QuestionFirstChoice = question.QuestionFirstChoice,
+                    QuestionSecondChoice = question.QuestionSecondChoice,
+                    QuestionThirdChoice = question.QuestionThirdChoice,
+                    QuestionFourthChoice = question.QuestionFourthChoice,
+                    ExamId = question.ExamId,
+                    RightAnswer = question.RightAnswer,
+                };
+                _repository.Insert(question2);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPut("update")]
+        public ActionResult UpdateQuestion(QuestionWithExam QwE)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = new Question()
+                {
+                    QuestionId = QwE.Id,
+                    QuestionHeader = QwE.QuestionHeader,
+                    QuestionFirstChoice = QwE.QuestionFirstChoice,
+                    QuestionSecondChoice = QwE.QuestionSecondChoice,
+                    QuestionThirdChoice = QwE.QuestionThirdChoice,
+                    QuestionFourthChoice = QwE.QuestionFourthChoice,
+                    RightAnswer = QwE.RightAnswer,
+                    ExamId = QwE.ExamId
+                };
+                _repository.Update(result);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult DeleteQuestion(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
